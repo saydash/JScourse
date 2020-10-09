fc3bdd5f364d75d342ce2cf8149e73f249bf2889
Saydash <saydash@ya.ru>
2020-10-09 17:23:35 +0300

    Basic Finally

--- js/script.js
+++ js/script.js
@@ -37,10 +37,17 @@ document.addEventListener('DOMContentLoaded',() => {
 
     addForm.addEventListener('submit', (e) => {
         e.preventDefault();
-        const newFilm = addInput.value;
+        let newFilm = addInput.value;
         const favorite = checkbox.checked;
         
         if (newFilm) {
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0,22)}...`;
+        
+            };
+            if (favorite) {
+                console.log('Добавили любимый фильм');
+            };
             movieDB.movies.push(newFilm);
             sortArr(movieDB.movies);
             createMovieList(movieDB.movies, movieList);
@@ -48,12 +55,7 @@ document.addEventListener('DOMContentLoaded',() => {
         
         
         e.target.reset();
-        // function checkLenght() {
-        //     if (newFilm.lenght >= 21) {
-        //         newFilm.slice(20);
-
-        //     };
-        // };
+        
     });    
 
     
@@ -81,12 +83,21 @@ document.addEventListener('DOMContentLoaded',() => {
 
     function createMovieList(films, parent) {
         movieList.innerHTML = "";
+        sortArr(films);
         films.forEach((film, i) => {
             parent.innerHTML += `
                 <li class="promo__interactive-item">${i + 1} ${film} 
                 <div class="delete"></div>
                 </li>
             `;
+        });
+
+        document.querySelectorAll(`.delete`).forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                createMovieList(movieDB.movies, movieList);
+            });
         });
     };
         
@@ -95,7 +106,7 @@ document.addEventListener('DOMContentLoaded',() => {
     // movieList.innerHTML = "";
     deleteAdv(promo);
     makeChanges();
-    sortArr(movieDB.movies);
+    
     createMovieList(movieDB.movies, movieList);
 
     
